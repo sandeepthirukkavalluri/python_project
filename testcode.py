9161c5f95cd2e27dd3fb36b4ac24f40084f8a5e9
sandeepsunny738 <sandeep.vf306@gmail.com>
2020-06-06 18:46:19 -0400

    Revert "last commit"
    This reverts commit 4eaf6382591dcdeaff3169d6840b38c9e4a53e20.

--- testcode.py
+++ testcode.py
@@ -9,17 +9,3 @@ def hash_file(filename):
    return h.hexdigest()
 message = hash_file("track1.mp3")
 print(message)
-
-
-
-def hash_file(filename):
-   h = hashlib.sha1()
-   with open(filename,'rb') as file:
-       chunk = 0
-       while chunk != b''
-           chunk = file.read(1024)
-           h.update(chunk)
-   return h.hexdigest()
-message = hash_file("track1.mp3")
-print(message)
-
