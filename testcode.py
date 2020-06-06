055a741b6bcdae194d6422cc4efcf166543a7f7f
sandeepthirukkavalluri <31046783+sandeepthirukkavalluri@users.noreply.github.com>
2020-06-06 18:08:54 -0400

    Update testcode.py

--- testcode.py
+++ testcode.py
@@ -4,8 +4,4 @@ def hash_file(filename):
    with open(filename,'rb') as file:
        chunk = 0
        while chunk != b''
-           chunk = file.read(1024)
-           h.update(chunk)
-   return h.hexdigest()
-message = hash_file("track1.mp3")
-print(message)
+           
